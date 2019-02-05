deb99f115fc92b3fac79d2e829140240448ca23e
notintheup <brianjandron@gmail.com>
2019-02-04 23:35:54 -0500

    final for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,48 @@
 //Global variables for Psychic game
 //---------------------------------- 
 var alphaChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//State variables
 var numberWins = 0;
 var numberLosses = 0;
-var guesses = 8;
-var remaining = 8;
 var userSelect = "";
 var macLetter = "";
-
+var turns = 9;
+//Variables by id
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var remaining = document.getElementById("remaining-text");
 var guessesText = document.getElementById("guesses-text");
-var choicesText = document.getElementById("choices-text");
+var leftText = document.getElementById("left-text");
 //Create event handler 
 document.onkeyup = (event) => {
+  turns--;
   userSelect = event.key.toLowerCase();
-  remaining--;
-
+  //Restart game
+  var restart = function () {
+    userSelect = ""; numberLosses = 0; numberWins = 0;
+    // macSelect();
+    }
   //Declared variable for Mac
   var macSelect = () => {
     macLetter = alphaChar[Math.floor(Math.random() * alphaChar.length)];
   }
-  // Declared variable for guesses
-  var guessesLeft = function () {
-    document.getElementById("remaining-text").innerHTML = "Guesses Left: " + left;
-  }
-  //Invoke Mic function
+  //Invoke Mac function
   macSelect();
 
   //Two men enter...one man leaves
   if (userSelect === macLetter) {
-    numberWins++;
-    remaining--;
+    numberWins = numberWins + 1;
   }
   if (userSelect != macLetter) {
-    numberLosses++;
-    remaining--;
-  } 
-  else if (remaining === 0) {
-    alert("You lose");
+    numberLosses = numberLosses + 1;
   }
-
+  if (turns === 0) {
+    alert("You lose!");
+    restart();
+  }
 
   winsText.textContent = "wins: " + numberWins;
   lossesText.textContent = "losses: " + numberLosses;
   guessesText.textContent = "You guess: " + userSelect;
-  remaining.textContent = remaining;
-  choicesText.textContent = "The computer chose: " + macLetter;
+  leftText.textContent = "Guesses remaining: " + turns;
 }
